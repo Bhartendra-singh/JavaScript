b5aab8d7733008b1d1cb576e0019ba061ee8a1e4
Bhartendra-singh <bhartendra750@gmail.com>
2026-03-07 21:45:57 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS code as newer version of js(old version js se likha hua code waste na ho)
+
+// alert(3 + 3)  // we can't use alert like that in nodejs. it will work in browser console but in node this is not working .there is a different syntax to use alert in node
+
+console.log(3 
+    +
+     3) // this type of code writing is not prefer , code readability should be high
+
+console.log("Ritik")
+
+let name = "Ritik"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+
+// number => 2 to power 52
+// bigInt => generally use in trading etc. for very large number
+// string => ""
+// boolean => true/false
+// null => standalone value hai(null means empty)
+// undefined => value not assigned
+// symbol => used for uniqueness (unique)
+
+
+//object => very important concept
+
+console.log(typeof age);
+
+console.log(typeof undefined); //output is undefined(undefined aik type h)
+console.log(typeof null);  //output is object
+
+
+
