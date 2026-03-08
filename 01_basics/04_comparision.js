87db9eed81d705e8fe1ef88fe307076fd758b0c0
Bhartendra-singh <bhartendra750@gmail.com>
2026-03-08 10:37:29 +0000

    comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,28 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+//console.log("2" > 1);   //output is true(we compare two different datatypes and when we compare different datatypes then output result is not expected)
+//console.log("02" > 1);  //output is true
+
+/*
+Mostly we avoid these type of comparisions b/c they create confusion
+*/
+console.log(null > 0);  //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+
+console.log(undefined == 0);    //false
+
+
+// === (strict check => check values and datatypes)
+
+console.log("2" === 1);
+
+
+
+
+
