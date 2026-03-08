82e67f71019d3fad1db257b146cc6c76e425f7f9
Bhartendra-singh <bhartendra750@gmail.com>
2026-03-08 07:45:16 +0000

    operations

--- 01_basics/03_conversionsOperations.js
+++ 01_basics/03_conversionsOperations.js
@@ -1,19 +1,19 @@
-let score = "33abc"
-let score1 = "33";
+// let score = "33abc"
+// let score1 = "33";
 
-let score2 = null
+// let score2 = null
 
-console.log(typeof score);
-console.log(typeof(score)); //another way
+// console.log(typeof score);
+// console.log(typeof(score)); //another way
 
-console.log(typeof score1);
+// console.log(typeof score1);
 
-let valueInNumber = Number(score)       //we can also use like this, now score is convert into number
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)       //we can also use like this, now score is convert into number
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-let valueInNumber1 = Number(score2)
-console.log(valueInNumber1);
+// let valueInNumber1 = Number(score2)
+// console.log(valueInNumber1);
 
 
 // "33" => 33
@@ -23,15 +23,61 @@ console.log(valueInNumber1);
 let isLoggedIn = "ritik"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 => output is true; 0 => false
 // "" => output is false
 // "ritik" => output is true 
 
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//*********** Operations *************//
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+let str1 = "hello"
+let str2 = " ritik"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log((3 + 4) * 5 % 3); //better way to write this type of code
+
+
+console.log(+true); //output is 1
+console.log(+"");   // output is 0(empty string convert into number)
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+//gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
