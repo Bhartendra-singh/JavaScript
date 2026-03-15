c0d92e6e98d7339d817941e68bc62760c4a11df3
Bhartendra-singh <bhartendra750@gmail.com>
2026-03-15 17:47:14 +0000

    while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,26 @@
+// while loop syntax
+
+// let index = 0 ;  //initializaton outside the loop hota h while loop mai.
+// while(condition){
+//     //block of code
+
+//     //increment
+// }
+
+// let index = 0
+
+// while(index <= 10){
+//     console.log(`value of index is ${index}`);
+
+//     index = index + 2;
+
+// }
+
+
+// do- while loop
+
+let score = 1
+do{
+    console.log(`Score is ${score}`);
+    score++;
+}while(score <= 10);
