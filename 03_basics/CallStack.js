57faa4ff406e840b588ad71579b79f2f7f1a394a
Bhartendra-singh <bhartendra750@gmail.com>
2026-03-15 13:19:56 +0000

    Execution context and call stack

---
+++ 03_basics/CallStack.js
@@ -0,0 +1,17 @@
+// JavaScript Execution Context
+
+// java script run our code in two phase
+
+// Global execution context always exist. Global execution ko allocate kiya jata h (this) ke andar.
+
+// JS is a single threaded language.
+
+// 1- Global execution context
+
+// 2- Function execution context
+
+// 3- Eval execution context
+
+// i => Memory creation phase
+// ii=> Execution phase 
+
