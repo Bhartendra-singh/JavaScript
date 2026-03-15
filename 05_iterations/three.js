53ae941a3e4df22a1214ecf05ab11e92358bfda6
Bhartendra-singh <bhartendra750@gmail.com>
2026-03-15 19:34:49 +0000

    For of loop in js

---
+++ 05_iterations/three.js
@@ -0,0 +1,53 @@
+
+// ["", "", ""]
+
+// [{}, {}, {}]
+
+
+// forof loop syntax
+
+// for (const element of object) {
+// 
+  //element=> variable name and object=> kis cheez pr loop lagana h like -> arr , object pr etc.
+    
+// }
+
+const arr = [1, 2, 3, 4, 5]
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+
+// const greetings = "Hello world!"
+// for(const greet of greetings){
+//     console.log(`Each char is ${greet}`);
+// }
+
+
+// Maps => is a object in js and it give unique values and give output in which order we enter the input.(maintain the order)
+
+const map = new Map()
+
+map.set('IN', "India")
+map.set('USA', "United states of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+// console.log(map);
+
+// for (const [key, value] of map) {
+//     console.log(key, ':-', value);
+// }
+
+
+
+const myObj = {
+    'game1': 'NFS',
+    'game2': 'Spiderman'
+}
+
+// for (const [key, value] of myObj) {
+//     console.log(key, ':-', value);
+// }
+
